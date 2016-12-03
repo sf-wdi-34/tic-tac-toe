72499dd1f8483468c93d5ad83a7eb8c0f577e86a
aquoss <amberquoss@gmail.com>
2016-12-03 14:21:48 -0800

    addClass to box when user clicks

--- js/app.js
+++ js/app.js
@@ -5,33 +5,31 @@ $(document).ready(function() {
 
   //assign symbols to players
   $('#x').on('click', function() {
-    var playerOne = x;
-    var playerTwo = o;
+    var playerOne = 'x';
+    var playerTwo = 'o';
   });
   $('#o').on('click', function () {
-    var playerOne = o;
-    var playerTwo = x;
+    var playerOne = 'o';
+    var playerTwo = 'x';
   });
 
   //determine who goes first
   if (playerOne) {
     var turn = randomlychoose(playerOne or playerTwo);
     if (turn === playerOne) {
-      $('#message').text('<h3>You go first Player One!</h3>')
+      $('#message').text('<h3>You go first Player One!</h3>');
     } else {
-      $('#message').text('<h3>You go first Player Two!</h3>')
+      $('#message').text('<h3>You go first Player Two!</h3>');
     }
   });
 
-  //even listener for player to click
+  //event listener for player to click
   var allBoxes = $('.column');
   allBoxes.on('click', addSymbol);
 
 });
 
 // assign symbol to X and symbol to O
-//create buttons, one with id X and the other id O
-//create message bar with h3 id message
 
 //keep track of who's turn it is
 function changeTurn(currentPlayer) {
@@ -52,15 +50,27 @@ function addSymbol() {
 }
 
 function playerOneSymbol() {
-  allBoxes.eq().css("background-image", playerOne);
-  changeTurn(playerOne);
-  $('#message').text('<h3>Your turn Player Two!</h3>');
+  //if box is empty then:
+    if (playerOne === 'x') {
+      allBoxes.eq().addClass("x-image");
+    } else {
+      allBoxes.eq().addClass("o-image");
+    }
+    changeTurn(playerOne);
+    $('#message').text('<h3>Your turn Player Two!</h3>');
+  //else message you can't do that
 }
 
 function playerTwoSymbol() {
-  allBoxes.eq().css("background-image", playerTwo);
-  changeTurn(playerTwo);
-  $('#message').text('<h3>Your turn Player One!</h3>');
+  //if box is empty then:
+    if (playerTwo === 'x') {
+      allBoxes.eq().addClass("x-image");
+    } else {
+      allBoxes.eq().addClass("o-image");
+    }
+    changeTurn(playerTwo);
+    $('#message').text('<h3>Your turn Player One!</h3>');
+  //else message you can't do that
 }
 
 //function to know when someone has won
